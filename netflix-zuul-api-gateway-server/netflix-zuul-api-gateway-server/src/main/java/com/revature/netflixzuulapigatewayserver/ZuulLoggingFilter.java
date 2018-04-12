81a06b827cbc398ed4721f4ec63fb18a98e0948f
AkshayaPk <ashpeekay23@gmail.com>
2018-04-12 09:03:44 -0400

    netflix-zuul-api-gateway-server commit

---
+++ netflix-zuul-api-gateway-server/netflix-zuul-api-gateway-server/src/main/java/com/revature/netflixzuulapigatewayserver/ZuulLoggingFilter.java
@@ -0,0 +1,48 @@
+package com.revature.netflixzuulapigatewayserver;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter{
+
+	private Logger logger=LoggerFactory.getLogger(this.getClass());
+	
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		logger.info("request-> {} request uri -> {}",request,request.getRequestURI());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		// TODO Auto-generated method stub
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 1;
+	}
+
+	/*
+	 * To run through API Gateway that is by default running on port 8765. All that we have to do is 
+	 * use the URI http://localhost:8765/{application-name}/{uri} for eg
+	 * http://localhost:8765/currency-exchange-service/currency-exchange/from/EUR/to/INR - This request will go through Zuul
+	 * */
+}
